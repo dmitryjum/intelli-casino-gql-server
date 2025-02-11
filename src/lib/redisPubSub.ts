0475c808bbc9c3519e9db762545552fc8963f5bf
Dmitry Jum <xfiler@ymail.com>
2025-02-11 15:18:23 -0800

    edits Redis PubSub to accomodate ElastiCache in prod environment

--- src/lib/redisPubSub.ts
+++ src/lib/redisPubSub.ts
@@ -1,14 +1,14 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import Redis, { RedisOptions } from 'ioredis';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Define Redis options dynamically
 const options: RedisOptions = {
-  host: process.env.REDIS_HOST || '127.0.0.1',
+  host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT || '6379', 10),
-  password: process.env.REDIS_PASSWORD || '',
-  retryStrategy: (times: number) => {
-    // reconnect after
-    return Math.min(times * 50, 2000);
-  }
+  ...(isProduction ? { tls: {} } : {}), // Enable TLS only for AWS
+  retryStrategy: (times: number) => Math.min(times * 50, 2000) // Retry strategy
 };
 
 export const pubsub = new RedisPubSub({
